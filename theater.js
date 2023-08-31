9b9338c1ed3002bdd3ddecb7aa1deab8bcdf8c39
Brian Tochterman <btoch23@gmail.com>
2023-08-31 13:12:11 -0500

    theatre exercise

---
+++ theater.js
@@ -0,0 +1,24 @@
+const GEN_ADMISSION_PRICE = 20;
+        const DISCOUNT_PRICE = 10;
+        const MATINEE_DISCOUNT = 3;
+
+        function buyTicket() {
+            const age = prompt('Enter age!');
+            let cost = getBaseTicketCost(age);
+            function getBaseTicketCost(age) {
+                if (age <= 12 || age >= 65) {
+                    return DISCOUNT_PRICE;
+                }
+                return GEN_ADMISSION_PRICE;
+            }
+            const isMatinee = prompt('Are you attending a matinee, yes or no?').toLowerCase();
+            cost = applyMatineeDiscount(cost, isMatinee);
+            alert(`Your ticket will cost $${cost}`);
+        }
+
+        function applyMatineeDiscount(cost, isMatinee) {
+            if (isMatinee === 'yes' || isMatinee === 'y') {
+                return cost - MATINEE_DISCOUNT;
+            }
+            return cost;
+        }
